751d4c9efcaa9717f36d5cc6f28840dc20a228ed
Thu Thuy <vietcong2k1@gmail.com>
2023-11-25 21:34:56 +0700

    add homework

--- src/main/java/Homework_AutoTest/XpathHomeWork_Day12.java
+++ src/main/java/Homework_AutoTest/XpathHomeWork_Day12.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -17,33 +18,21 @@ public class XpathHomeWork_Day12 extends CommonBase {
 	@Test
 	public void dropdownlistState () throws InterruptedException {
 		WebElement selectTag = driver.findElement(By.name("state"));
+		WebElement textcheckbox = driver.findElement(By.xpath("//label[text()='Do you have hosting?']"));
+		WebElement radioYes = driver.findElement(By.xpath("//input[@value='yes']"));
+
 		Select dropdownlist = new Select(selectTag);
-		//chọn Hawaii bằng cách VisibleText
 		selectTag.click();
 		dropdownlist.selectByVisibleText("Hawaii");
+		textcheckbox.click();
+		radioYes.click();
+		Thread.sleep(10000);
 		System.out.println("dropdownlist first option is: "+dropdownlist.getFirstSelectedOption().getText());
-	}
-	
-	@Test
-	public void testCheckbox() {
-		//kiểm tra giá trị mặc định của radio  yes và no là chưa check
-		WebElement radioYes = driver.findElement(By.xpath("//input[@value='yes']"));
-		WebElement radioNo = driver.findElement(By.xpath("//input[@value='no']"));
-		System.out.println("trạng thái của radio yes là "+radioYes.isSelected());
-		System.out.println("trạng thái của radio no là "+radioNo.isSelected());
-		
-		//kiểm tra radio chưa check thì click vào
-		if (radioYes.isEnabled()==true && radioNo.isSelected()==false) {
-			radioYes.click();
-		}
-		else if (radioNo.isEnabled()==true && radioYes.isSelected()==false) {
-			radioNo.click();
-		
-		}
 	}
 	
 	@AfterMethod
 	public void closeChrome() {
 		driver.close();
 	}
+
 }
